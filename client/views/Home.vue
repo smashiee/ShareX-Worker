61609b6f2655b2b42c43c2a71add689a854de83f
aStonePenguin <6313418+aStonePenguin@users.noreply.github.com>
2025-03-29 08:59:45 -0700

    Move config downloader to top

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -10,12 +10,14 @@ apiStore.isPageLoaded = true;
 </script>
 
 <template>
-	<div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-		<ShareHistory/>
-	</div>
-
-	<!-- Just plop this here at the bottom! -->
+	<!-- Just plop this here at the top! -->
 	<div class="flex min-h-100 flex-col items-center justify-center bg-background md:p-5">
 		<ConfigDownloader/>
 	</div>
+
+	<div class="flex flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+		<ShareHistory/>
+	</div>
+
+
 </template>
