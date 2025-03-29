ebc0300d160379606ca369a430b6174b5132c148
aStonePenguin <6313418+aStonePenguin@users.noreply.github.com>
2025-03-29 08:36:59 -0700

    Unnecessarily fancy file upload

--- client/views/Upload.vue
+++ client/views/Upload.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
+import {
+	Card,
+	CardContent,
+	CardFooter
+} from '@/components/ui/card';
+
+import {
+	FileUpload,
+	FileUploadGrid
+} from '@/components/ui/file-upload';
+
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+
 import { ref } from 'vue';
 
 import { useApiStore } from '@/stores/api';
 const apiStore = useApiStore();
 apiStore.isPageLoaded = true;
 
-const fileInput = ref<HTMLInputElement | null>(null)
-const file = ref<File | null>();
-const onFileChanged = async (e: any) => {
-	const target = e.target as HTMLInputElement;
-	const files = target.files;
 
-	if (!files || !files[0])
+const file = ref<File>();
+const onFileChanged = async (files: File[]) => {
+	if (!files || files.length === 0)
 		return;
-
 
 	file.value = files[0];
 }
@@ -38,13 +43,11 @@ const onSubmit = async () => {
 	<div class="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
 		<div class="w-full max-w-sm">
 			<Card class="w-full max-w-sm">
-				<CardHeader class="text-center">
-					<CardTitle class="text-2xl">Upload File</CardTitle>
-					<CardDescription>Choose a file to share.</CardDescription>
-				</CardHeader>
-				<CardContent class="grid gap-4">
-					<Label for="file">File:</Label>
-					<Input ref="fileInput" id="file" type="file" @change="onFileChanged" />
+
+			<CardContent class="grid gap-4">
+				<FileUpload @on-change="onFileChanged">
+					<FileUploadGrid />
+				</FileUpload>
 				</CardContent>
 				<CardFooter>
 					<Button @click="onSubmit" class="w-full">Upload</Button>
@@ -52,5 +55,4 @@ const onSubmit = async () => {
 			</Card>
 		</div>
 	</div>
-
 </template>
